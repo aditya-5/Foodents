7550d49b3412bd23d0154c6755176389ece34e79
Sitri08 <52980207+Sitri08@users.noreply.github.com>
2021-03-03 16:25:04 +0200

    Confirmed DB consistency

--- database work/parse_json.py
+++ database work/parse_json.py
@@ -1,68 +1,38 @@
 # I could not find a suitable online JSON to CSV parser (or to any other format, really), 
 # so I'm doing it manually
 
-import re
 from recipe import Recipe
+from entries import Entries as e
 
-def parseRecipeText(r, recipeText):
-    i = 0
-    while i < len(recipeText):
-        if recipeText[i] == "\"ingredients\": [": 
-            while(recipeText[i] != "],"):
-                print(recipeText[i])
-                i += 1
-        elif recipeText[i] == "\"tags\": [": 
-            while(recipeText[i] != "]"):
-                print(recipeText[i])
-                i += 1
-        else:
-            addParameter(r, recipeText[i])
-        i += 1
-        # else:
-        #     print(recipeText[i])
-        # if recipeText[i] == "\"ingredients\": [":
-        #     while(recipeText[i] != "],"):
-        #         i += 1
-        # elif recipeText[i] == "\"tags\": [":
-        #     while(recipeText[i] != "]"):
-        #         i += 1
-        # else:
-        #     addParameter(r, recipeText[i])
+def printKeys():
 
-    return r
+    print("Total keys in DB: ", totalKeys)
+    print("Keys common to all entries: ", commonKeys)
+    print("Inconsistent keys: ", [k for k in totalKeys if k not in commonKeys])
 
-def addParameter(r, line):
-    name = ""
-    i = 1
-    print(line)
-    while line[i] != "\"":
-        name += line[i]
-        i += 1
-    print(name)
+def findKeys():
+    commonKeys = list(e.entries["2"].keys())
+    totalKeys = []
 
-recipes = []
+    for entry in e.entries:
+        entry = e.entries[entry]
+        keys = entry.keys()
+        for key in keys:
+            if key not in totalKeys:
+                totalKeys.append(key)
+        for key in commonKeys:
+            if key not in keys:
+                commonKeys.remove(key)
 
-with open("db_test.json", "r") as f:
-    line = 1
-    while line:
-        line = f.readline().strip()
+    return (totalKeys, commonKeys)
 
-        regex = "^\"(id)?\d{1,3}\": \{$"
-        found = re.search(regex, line)
-        if found:
-            regex = "(id)?\d{1,3}"
-            found = re.search(regex, line)
-            r = Recipe(found[0])
+if __name__ == "__main__":
+    (totalKeys, commonKeys) = findKeys()
 
-            recipeText = []
+    # conclusion: all entries have the same structure
+    # printKeys()
 
-            line = f.readline().strip()
-            while line != "}" and line != "},":
-                recipeText.append(line)
-                line = f.readline().strip()
 
-            recipes.append(parseRecipeText(r, recipeText))
 
-print()
-for r in recipes:
-    print(r.id, end=" ")
+    
+
