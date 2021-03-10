d966a8dfaa72808dffe26506c3a500cfcc9a8902
Aditya Agarwal <aditya.7@outlook.com>
2021-03-11 02:06:47 +0530

    Modified navbar

--- login/login.php
+++ login/login.php
@@ -58,6 +58,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 							$_SESSION['first_name'] = $firstn;
 							$_SESSION['last_name'] = $lastn;
 							$_SESSION['email'] = $email;
+							// header("location: welcome.php");
 							header("location: welcome.php");
 						}
 						else{
