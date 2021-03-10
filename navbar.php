8865ed6ed0818f24c6653eb1136a4187f4e15c49
Aditya Agarwal <aditya.7@outlook.com>
2021-03-11 05:25:53 +0530

    Moved metadata and assets to navbar component

--- navbar.php
+++ navbar.php
@@ -10,6 +10,43 @@ if(isset($_SESSION['loggedin'])){
 }
  ?>
 
+ <head>
+  <!-- Metadata -->
+   <meta charset="utf-8">
+   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+   <meta content="" name="description">
+   <meta content="" name="keywords">
+
+   <!-- Favicons -->
+   <link href="assets/img/logo.png" rel="icon">
+   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+   <!-- Google Fonts -->
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+   <!-- Vendor CSS Files -->
+   <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+   <!-- Template Main CSS File -->
+   <link href="assets/css/style.css" rel="stylesheet">
+
+   <!-- jQuery Local -->
+   <script src="jquery-3.5.1.min.js"></script>
+
+   <!-- MDB ICONS -->
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css" rel="stylesheet"/>
+
+   <!-- Font Awesome Icons -->
+   <script src="https://kit.fontawesome.com/21dbb8851c.js" crossorigin="anonymous"></script>
+ </head>
+
 <header id="header" class="fixed-top">
       <div class="container d-flex align-items-center">
 
