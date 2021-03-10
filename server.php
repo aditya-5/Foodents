1d4c23adf4173934cbb22918f37d9ce06e7cdfe4
Aditya Agarwal <aditya.7@outlook.com>
2021-03-11 02:10:30 +0530

    Transition from html to php files

---
+++ server.php
@@ -0,0 +1,27 @@
+<?php
+require_once('config.inc.php');
+
+
+$con = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+if($con -> connect_error) {
+    die('Connect Error ('.$con -> connect_errno.') '.$con -> connect_error);
+}
+
+$sql = "INSERT INTO USERS (username, password, email, first_name, last_name)
+VALUES ('admin', '1234', 'admin@manchester.ac.uk', 'Mr. Admin', 'Realperson')";
+
+if($con->query($sql) === FALSE) echo "Error: " . $sql . "<br>" . $con->error;
+else
+{
+  echo "Admin added successfully<br>";
+
+  $sql = "SELECT * FROM USERS";
+
+  if ($result = $con -> query($sql)) {
+    echo "Returned rows are: " . $result -> num_rows;
+    $result -> free_result();
+  }
+}
+$con -> close();
+?>
