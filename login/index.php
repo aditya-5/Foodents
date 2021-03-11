a5f15339a7ac9308df9c10ef7ab5a68eebc7440e
Aditya Agarwal <aditya.7@outlook.com>
2021-03-11 18:08:25 +0530

    Fixed db connection error

--- login/index.php
+++ login/index.php
@@ -4,20 +4,20 @@
 
 <?php
 
-// require_once('../config.inc.php');
+require_once('../config.inc.php');
 // For local database only
-define("DB_SERVER",'localhost');
-define("DB_USERNAME", 'root');
-define("DB_PASSWORD", '');
-define("DB_NAME", '2020_comp10120_x9');
+// define("DB_SERVER",'localhost');
+// define("DB_USERNAME", 'root');
+// define("DB_PASSWORD", '');
+// define("DB_NAME", '2020_comp10120_x9');
 
 // First run (Local)
 // $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD);
 
 // If database already created (Local)
-$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_NAME);
+// $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_NAME);
 
-// $conn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+$conn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
 
 if($conn === false){
 	die("ERROR: COULDN'T CONNECT TO DATABASE " .  mysqli_connect_error());
