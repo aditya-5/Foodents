408dd44727557761d29153bd686a55417f57868d
Aditya Agarwal <aditya.7@outlook.com>
2021-03-11 14:34:52 +0530

    Added error 404 page and hided php in url, added .htaccess

---
+++ error.php
@@ -0,0 +1,43 @@
+<div class="sidebar">
+
+              <h3 class="sidebar-title"><a href="search.html"> <- Go Back</h3><br></a>
+           
+
+           
+
+              <h3 class="sidebar-title">Recent Recipes</h3>
+              <div class="sidebar-item recent-posts">
+                <div class="post-item clearfix">
+                  <img src="assets/img/recipeimg/biscuit.jpg" alt="">
+                  <h4><a href="recipe1.html">Flaky Buttermilk Biscuits</a></h4>
+                  <time datetime="2020-01-01">March 5, 2020</time>
+                </div>
+
+                <div class="post-item clearfix">
+                  <img src="assets/img/recipeimg/salad.jpg" alt="">
+                  <h4><a href="recipe1.html">Greek Salad</a></h4>
+                  <time datetime="2020-01-01">March 5, 2020</time>
+                </div>
+
+                <div class="post-item clearfix">
+                  <img src="assets/img/recipeimg/cake.jpg" alt="">
+                  <h4><a href="recipe1.html">Godiva Dark Chocolate Cheesecake</a></h4>
+                  <time datetime="2020-01-01">February 1, 2020</time>
+                </div>
+
+
+
+              </div><!-- End sidebar recent posts-->
+
+              <h3 class="sidebar-title">Tags</h3>
+              <div class="sidebar-item tags">
+                <ul>
+                  <li><a href="#">Dessert</a></li>
+                  <li><a href="#">Biscuits</a></li>
+                  <li><a href="#">Italian</a></li>
+                  <li><a href="#">Chinese</a></li>
+                  <li><a href="#">Easy</a></li>
+                </ul>
+              </div><!-- End sidebar tags-->
+
+            </div><!-- End sidebar -->
