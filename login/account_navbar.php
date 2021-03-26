219b512e50142e988ecb4bb3dde53af27e00e855
Aditya Agarwal <aditya.7@outlook.com>
2021-03-26 21:11:02 +0530

    Modified navbars, removed useless assets, imported cdns

--- login/account_navbar.php
+++ login/account_navbar.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
-$log = false;
-if(isset($_SESSION['loggedin'])){
-  if($_SESSION['loggedin']==true){
-  	$fname = $_SESSION['first_name'];
-    $lname = $_SESSION['last_name'];
-    $log = true;
-  }
-}
- ?>
+
 
  <head>
   <!-- Metadata -->
@@ -18,30 +8,26 @@ if(isset($_SESSION['loggedin'])){
    <meta content="" name="keywords">
 
    <!-- Favicons -->
-   <link href="assets/img/logo.png" rel="icon">
-   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+   <link href="../assets/img/logo.png" rel="icon">
+   <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
    <!-- Google Fonts -->
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-   <!-- Vendor CSS Files -->
-   <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
    <!-- Bootstrap JS File -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
    <!-- Template Main CSS File -->
-   <link href="assets/css/style.css" rel="stylesheet">
+   <link href="../assets/css/style.css" rel="stylesheet">
 
    <!-- jQuery Local -->
-   <script src="jquery-3.5.1.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.slim.js"></script>
 
    <!-- MDB ICONS -->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css" rel="stylesheet"/>
@@ -64,32 +50,33 @@ if(isset($_SESSION['loggedin'])){
 
 
         <nav id="navbar" class="navbar order-last order-lg-0">
-          <a href="index" class="active">Home</a>
+          <a href="../index" class="active">Home</a>
 
           <span class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="about">About Us</a></li>
-              <li><a href="team">Team</a></li>
-              <li><a href="testimonials">Testimonials</a></li>
+              <li><a href="../about">About Us</a></li>
+              <li><a href="../team">Team</a></li>
+              <li><a href="../testimonials">Testimonials</a></li>
             </ul>
           </span>
 
-          <a href="fridge">Fridge</a>
-          <a href="search">Search Recipe</a>
-          <a href="contact">Contact</a>
+          <a href="../fridge">Fridge</a>
+          <a href="../search">Search Recipe</a>
+          <a href="../contact">Contact</a>
           <?php
             if($log){
-              echo "<span class='dropdown'><a href='#'><span>".$fname." ".$lname."</span> <i class='bi bi-chevron-down'></i>."&nbsp&nbsp&nbsp&nbsp".</a>
+              echo "<span class='dropdown'><a href='#'><span>".$fname." ".$lname."</span> <i class='bi bi-chevron-down'></i>"."&nbsp&nbsp&nbsp&nbsp"."</a>
                 <ul>
-                  <li><a href='./login/profile'>Profile</a></li>
-                  <li><a href='./login/logout'>Logout</a></li>
+                  <li><a href='./profile'>Profile</a></li>
+                  <li><a href='./logout'>Logout</a></li>
                 </ul>
               </span>";
             }
             else{
-              echo "<a href='./login/login'>Login</a>";
+              echo "<a href='./login'>Login</a>";
             }
            ?>
+
         </nav>
 
       </div>
