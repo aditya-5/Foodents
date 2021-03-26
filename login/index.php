caaa44706057cb2a21190ce749659776e2b897d2
Aditya Agarwal <aditya.7@outlook.com>
2021-03-26 20:19:35 +0530

    Login revert

--- login/index.php
+++ login/index.php
@@ -4,12 +4,12 @@
 
 <?php
 
-// require_once('../config.inc.php');
+require_once('../config.inc.php');
 // For local database only
-define("DB_SERVER",'localhost');
-define("DB_USERNAME", 'root');
-define("DB_PASSWORD", '');
-define("DB_NAME", '2020_comp10120_x9');
+// define("DB_SERVER",'localhost');
+// define("DB_USERNAME", 'root');
+// define("DB_PASSWORD", '');
+// define("DB_NAME", '2020_comp10120_x9');
 
 // First run (Local)
 // $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD);
@@ -17,7 +17,7 @@ define("DB_NAME", '2020_comp10120_x9');
 // If database already created (Local)
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_NAME);
 
-// $conn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+$conn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
 
 if($conn === false){
 	die("ERROR: COULDN'T CONNECT TO DATABASE " .  mysqli_connect_error());
