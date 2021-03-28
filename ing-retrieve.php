9f700fd19dc3c95d54524d812380909f25290c04
Aditya Agarwal <aditya.7@outlook.com>
2021-03-29 03:24:36 +0530

    Fixed JSON empty string bug

--- ing-retrieve.php
+++ ing-retrieve.php
@@ -8,7 +8,19 @@ foreach($result as $value){
   array_push($ings, $value['name']);
 }
 
-echo json_encode($ings);
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+echo json_encode(utf8ize($ings));
+
 
 
  ?>
