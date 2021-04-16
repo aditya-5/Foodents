7a635836184f0623ca9941915b989fbad2e660f4
Sitri08 <52980207+Sitri08@users.noreply.github.com>
2021-04-16 18:18:06 +0300

    Bug fix

--- sidebar.php
+++ sidebar.php
@@ -9,19 +9,19 @@
               <div class="sidebar-item recent-posts">
                 <div class="post-item clearfix">
                   <img src="assets/img/recipeimg/biscuit.jpg" alt="">
-                  <h4><a href="recipe1.html">Flaky Buttermilk Biscuits</a></h4>
+                  <h4><a href="./recipe?id=631">Flaky Buttermilk Biscuits</a></h4>
                   <time datetime="2020-01-01">March 5, 2020</time>
                 </div>
 
                 <div class="post-item clearfix">
                   <img src="https://hispanickitchen-nglmedia.netdna-ssl.com/wp-content/uploads/2017/07/Ensalada-de-Pollo_1500-700x500.jpg" alt="">
-                  <h4><a href="recipe1.html">Mexican Ensalada</a></h4>
+                  <h4><a href="./recipe?id=635">Mexican Ensalada</a></h4>
                   <time datetime="2020-01-01">March 5, 2020</time>
                 </div>
 
                 <div class="post-item clearfix">
                   <img src="http://themessybaker.com/wp-content/uploads/2013/04/salmon-ebelskivers.gif" alt="">
-                  <h4><a href="recipe1.html">Smoked Salmon Ebelskivers</a></h4>
+                  <h4><a href="./recipe?id=623">Smoked Salmon Ebelskivers</a></h4>
                   <time datetime="2020-01-01">February 1, 2020</time>
                 </div>
 
