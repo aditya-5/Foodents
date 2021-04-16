4ea883a51af8e01e9ce3a22df8d111dd1cf9cb3f
Aditya Agarwal <aditya.7@outlook.com>
2021-04-16 14:56:24 +0530

    Refactored db connection

--- server.php
+++ server.php
@@ -1,8 +1,40 @@
 <?php
+
+// ******************************
+// FOR MANCHESTER SERVER DEPLOYMENT
+// ******************************
 require_once('config.inc.php');
-
-
 $con = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+// ******************************
+// FOR LOCAL DEVELOPMENT
+// ******************************
+// define("DB_SERVER",'localhost');
+// define("DB_USERNAME", 'root');
+// define("DB_PASSWORD", '');
+// define("DB_NAME", '2020_comp10120_x9');
+// $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_NAME);
+
+
+// ******************************
+// ONLY FOR HEROKU DEPLOYMENT
+// ******************************
+//Get Heroku ClearDB connection information
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+//
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+// ******************************
+// EXTRAS
+// ******************************
 //
 // if($con -> connect_error) {
 //     die('Connect Error ('.$con -> connect_errno.') '.$con -> connect_error);
@@ -36,30 +68,5 @@ $con = new mysqli($database_host, $database_user, $database_pass, $group_dbnames
 // else{
 // 	$msgs = "Connected Successfully";
 // }
-
-// ******************************
-// FOR LOCAL DEVELOPMENT
-// ******************************
-// define("DB_SERVER",'localhost');
-// define("DB_USERNAME", 'root');
-// define("DB_PASSWORD", '');
-// define("DB_NAME", '2020_comp10120_x9');
-// $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_NAME);
-
-
-// ******************************
-// ONLY FOR HEROKU DEPLOYMENT
-// ******************************
-//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-//
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
