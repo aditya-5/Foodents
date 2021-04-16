c04cc5fa0ef8d95b6c03b9d4b281cdc75918ba05
Sitri08 <52980207+Sitri08@users.noreply.github.com>
2021-04-16 16:36:15 +0300

    Test

--- fridge.php
+++ fridge.php
@@ -45,6 +45,56 @@ if(isset($_SESSION["error"])){
 <section id="contact" class="contact">
       <div class="container">
         <div class="section-title" data-aos="fade-up" style = "padding-bottom: 0px">
+            <h2>Fridge</h2>
+          </div>
+          <div class="row mt-5 justify-content-center" data-aos="fade-up">
+            <div class="col-lg-10">
+
+              <?php
+              if(isset($error)){
+              echo "<div class='alert alert-danger' role='alert'>".$error."</div>";
+              }
+              ?>
+              <form method="get" id="fridgeRes" action="results.php" class="php-email-form">
+                <div class="row">
+                  <div class="col-md-12 form-group">
+                    <input type="text"  id="ings" class="form-control" placeholder="Search and select ingredients from the table" >
+                  </div>
+
+                <div class="row d-flex justify-content-center">
+                  <div class="col-lg-4" >
+
+                      <table class="table">
+
+                        <thead>
+                          <tr><th scope="col"><center>Ingredient Name</center></th></tr>
+                        </thead>
+
+                        <tbody id="displayIng">
+                          <tr><td><center><span id="opti">Click on ingredients to select them</span></center></td></tr>
+                        </tbody>
+                      </table>
+                  </div>
+                </div>
+                <div class="text-center" style = "padding-top: 10px">
+                  <button type="submit" name="fridgeRes" value="submit">Search</button></div>
+
+                  <input type="text" hidden name="selectedIngs" id="selectedIngs" value="">
+              </form>
+              <br><br><br>
+              <div id="selectedIng">
+              </div>
+
+
+
+
+            </div>
+
+          </div>
+      </div>
+
+      <div class="container">
+        <div class="section-title" data-aos="fade-up" style = "padding-bottom: 0px">
           <h2>Fridge</h2>
         </div>
         <div class="row mt-5 justify-content-center" data-aos="fade-up">
