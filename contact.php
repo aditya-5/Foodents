8facbee5a852aeae0ce443becdab06051ae8a1d9
Sitri08 <52980207+Sitri08@users.noreply.github.com>
2021-04-27 20:18:58 +0300

    Fixed issue with contacts

--- contact.php
+++ contact.php
@@ -78,7 +78,7 @@
 
         <div class="row mt-5 justify-content-center" data-aos="fade-up">
           <div class="col-lg-10">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <form action="contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
--- database work/insert_ingredients.py
+++
@@ -1,8 +0,0 @@
-from entries import Entries as e
-
-def checkNameDuplicates():
-    
-
-
-if __name__ == "__main__":
-    checkNameDuplicates()
